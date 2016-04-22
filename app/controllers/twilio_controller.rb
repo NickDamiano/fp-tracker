826db59d76906f257707bde1201a75fae9ceb8a5
Nick Damiano <damiano.nick@gmail.com>
2016-04-22 11:28:14 +0300

    Changes the message to rocio centric

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -10,7 +10,7 @@ class TwilioController < ApplicationController
  
   def voice
   	response = Twilio::TwiML::Response.new do |r|
-  	  r.Say 'Hey there. Congrats on integrating Twilio into your Rails 4 app.', :voice => 'alice'
+  	  r.Say 'Hi Sexy Rocio. You are so sexy and i love you.', :voice => 'alice'
          r.Play 'http://linode.rabasa.com/cantina.mp3'
   	end
  
