c8e95602f9b69f9e60749bdbe46ed682ac2a70ed
Nick Damiano <damiano.nick@gmail.com>
2016-05-05 23:33:59 +0300

    Adds twilio phone to secrets with environmental variable reference

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,12 +2,9 @@ class NotificationsController < ApplicationController
 
 	skip_before_action :verify_authenticity_token
 
-	def notify
-		sid = ENV["TWILIO_ACCOUNT_SID"]
-		token = ENV["TWILIO_AUTH_TOKEN"]
-		client = Twilio::REST::Client.new(sid, token)
-		message = client.account.messages.create from: '+19037513020', to: '+19032749986', body: 'Learning to send SMS you are.', media_url: 'http://linode.rabasa.com/yoda.gif'
-		render plain: message.status
+	def send_message(phone)
+		
+		
 	end
 
 end
