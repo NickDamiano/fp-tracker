0c5f94488744ca61d3ecd7ec660481fe1ad2c2cc
Nick Damiano <damiano.nick@gmail.com>
2016-05-06 16:18:28 +0300

    Adds case statement for receiving and parsing text with stubbed code. also stubbs store_message message method

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,8 +3,29 @@ class NotificationsController < ApplicationController
 	skip_before_action :verify_authenticity_token
 
 	def parse
-		#call this 
+		# log message to history
 		p params["Body"]
+		message = params["Body"]
+		message.downcase!
+		case message
+		when /^911/
+			p "It's an emergency"
+		when /^sitrep/
+			p "It's requesting sitrep"
+		else
+			p "It's a depart/arrive"
+		end
+		# case statement for 
+		# emergency
+		# mass distro message for TASS or for king air guys
+		# SITREP reports on who is where. lists out location
+			# followed by indented names 
+		# LFG feature later
+		# else it's a report for departing or arriving
+
 	end
+
+
+
 
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,14 @@
 class Message < ActiveRecord::Base
 	belongs_to :employee
+
+	def store_message(names, location_from, location_to)
+		#loop through names array
+			# Find record by last name. if more than one result is
+				# returned, implenet duplicate method to send follow-up
+				# text
+			#if record is only 1 result found update status transit to location
+			#if not found, regex it to see a similar name and send follow-up
+			
+
+	end
 end
