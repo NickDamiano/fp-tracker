84313c3699b87350d638cbb77b662d0ec0f3cb9a
Nick Damiano <damiano.nick@gmail.com>
2016-05-07 21:40:05 +0300

    Creates a readme with brief description

--- app/models/message_actions.rb
+++ app/models/message_actions.rb
@@ -4,13 +4,19 @@ class MessageActions
 	def self.depart(message)
 		puts 'depart was totally called broski'
 		names = parse_names(message)
-		from = parse_location_from(message)
 		to = parse_location_to(message)
-		result = { names: names, from: from, to: to }
+		result = { names: names, to: to }
 	end
 
 	def self.arrive(message)
 		puts "arrival message_actions called"
+	end
+
+	def self.parse_location_to(message)
+		# if the message contains the word to 
+		if message =~ /\sto\s/
+			return message.split('to')[-1].strip!
+		end
 	end
 
 	def self.emergency(message, sender)
@@ -24,6 +30,8 @@ class MessageActions
 
 	def self.parse_names(message)
 		# message is ["bart, lisa, marge left al yamama to psab"]
+		# remove "and"
+		message = message.gsub!(/and/, '')
 		first = message.split(',')
 		# gets last name and pushes them all together. 
 		# Returns ["bart, lisa, marge"]
@@ -32,18 +40,9 @@ class MessageActions
 		first[0...-1].each{ | name | name.strip!}.push(last_name)
 	end
 
-	def self.parse_location_from(message)
-		'from location'
-	end
-
 	def self.forward_unparsed(message, sender)
 		# build and send message with TwiML back to sender
 	end
 
-	def self.parse_location_to(message)
-		# if the message contains the word to 
-		if message =~ /\sto\s/
-			return message.split('to')[-1].strip!
-		end
-	end
+	
 end
