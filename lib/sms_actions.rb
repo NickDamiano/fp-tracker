591a3d055f523d2b1fa8f520ab51dc093bb046ea
Nick Damiano <damiano.nick@gmail.com>
2016-07-25 16:08:39 +0300

    modifies smsactions with rails secrets

--- lib/sms_actions.rb
+++ lib/sms_actions.rb
@@ -5,14 +5,14 @@ class SmsActions
 	# should this be a hash as one variable
 	def self.compose_message(to, from, body)
 		p "in compose message"
-		# account_sid = Rails.application.secrets.twilio_account_sid
+		account_sid = Rails.application.secrets.twilio_account_sid
 		# auth_token = Rails.application.secrets.twilio_auth_token
-		account_sid = ENV["TWILIO_ACCOUNT_SID"]
+		# account_sid = ENV["TWILIO_ACCOUNT_SID"]
 		auth_token = ENV["TWILIO_AUTH_TOKEN"]
 
 		@client = Twilio::REST::Client.new(account_sid, auth_token)
 		p 'client created'
-		p "account sid is #{account_sid}"
+
 		message = @client.account.messages.create({
 			from: from,
 			to: to,
