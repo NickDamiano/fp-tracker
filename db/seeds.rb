239fc00a9d0ce7f206f93e168206ce00a337fa32
Nick Damiano <damiano.nick@gmail.com>
2016-10-10 08:44:36 +0300

    ack message for arrived works but depart only works when there are no duplicates

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ employees = Employee.create([
 	{ first_name: 'not in the system', in_country: true, permanent: true},
 	{ first_name: 'twilio_app', last_name: "twilio_app", phone_num1: twilio_number, 
 	phone_num2:  twilio_test_number},
-	{ first_name: 'nick', last_name: 'damiano', phone_num1: "+15129605956", location: 'outer rim', 
+	{ first_name: 'nick', last_name: 'damiano',  phone_num1: "+15129605956", location: 'outer rim', 
 		in_country: true, admin: true},
 	{ first_name: 'anakin', last_name: 'skywalker', phone_num1: "+15121112222", location: 'naboo', 
 		in_country: true, admin: false},
