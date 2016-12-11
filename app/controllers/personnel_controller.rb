2be00b50f7e21bb2576d4852de8c725d8ee01619
Nick Damiano <damiano.nick@gmail.com>
2016-12-11 22:32:01 +0900

    styles front end with sortable tables and add instructions to text field.

--- app/controllers/personnel_controller.rb
+++ app/controllers/personnel_controller.rb
@@ -1,5 +1,13 @@
 class PersonnelController < ApplicationController
 	def show
-		@personnel = Employee.where(in_country: true).order(:last_name, :first_name)
+		@personnel = Employee.where(in_country: true).order(sort_column)
+	end
+
+	private
+
+	# Sanitize input for params to prevent SQL injection
+	# If our products column name includes that parameters name
+	def sort_column
+		Employee.column_names.include?(params[:sort]) ? params[:sort] : "name"
 	end
 end
